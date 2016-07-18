a8e3d193e7c9ab45a6863c84db0c92ae6edee633
Iman Encarnacion <iman@syvlabs.com>
2016-07-19 00:10:45 +0800

    add initial google docs integration

--- index.js
+++ index.js
@@ -1,40 +1,50 @@
 var Promise = require("bluebird");
 var config = require('config');
 var Slack = require('slack-node');
+var GoogleSpreadsheet = require('google-spreadsheet');
+var googleCreds = require('./google-generated-creds.json');
 
-var slack = null;
-var users = null;
-var userToLog = null;
-var imToLog = null;
+var users;
+var userToLog;
+var imToLog;
 var messagesLog = [];
-var getApiToken = new Promise(function(resolve, reject){
-    var apiToken = config.get('Slack.config.apiToken');
-    if (!apiToken) {
-        console.log('No apiToken found in config file. Exiting...');
-        process.exit();
-    } else {
-        slack = Promise.promisifyAll(new Slack(apiToken));
-        resolve();
-    }
-});
-var getMessages = function (latest) {
+var sheet;
+var lastTs;
+var slack = Promise.promisifyAll(new Slack(config.get('Slack.config.apiToken')));
+var doc = Promise.promisifyAll(new GoogleSpreadsheet(config.get('App.sheetId')));
+
+var getMessages = function (oldest, latest) {
     slack.api("im.history", {
         channel: imToLog.id,
-        latest: latest
+        latest: latest,
+        oldest: oldest
     }, function (err, response) {
         messagesLog = messagesLog.concat(response.messages);
         if (response.has_more)
-            getMessages(messagesLog[messagesLog.length - 1].ts);
+            getMessages(oldest, messagesLog[messagesLog.length - 1].ts);
         } else {
-            console.log("done");
+            saveRowsToDoc();
         }
     });
 };
 
-//TODO: Add google docs integration, https://www.npmjs.com/package/google-spreadsheet
+var saveRowsToDoc = function () {
+    for (var i = messagesLog.length - 1; i >= 0; i--) {
+        var message = messagesLog[i];
+        
+    }
+};
 
-getApiToken
-    .then(() => slack.apiAsync("users.list"))
+doc.useServiceAccountAuthAsync(googleCreds)
+    .then(() => doc.getInfoAsync())
+    .then(function (spreadsheet) {
+        sheet = spreadsheet.worksheets[0];
+        return sheet.getRowsAsync({limit: 1});
+    })
+    .then(function (rows) {
+        lastTs = rows[0]['b'].numericValue;
+        return slack.apiAsync("users.list")
+    })
     .then(function (response) {
         var name = config.get('App.usernameToLog');
         users = response.members;
@@ -43,22 +53,8 @@ getApiToken
     })
     .then(function (response) {
         imToLog = response.ims.find((im) => (im.user === userToLog.id));
-        getMessages();
+        getMessages(lastTs);
     })
     .catch(function(error){
         console.log(error);
     });
-
-// slack.api("users.list", function(err, response) {
-//     for (var i = 0; i < response.members.length; i++) {
-//         var member = response.members[i];
-//         console.log(member.id+": "+member.name);
-//     }
-// });
-
-// slack.api('chat.postMessage', {
-//     text:'hello from nodejs',
-//     channel:'#general'
-// }, function(err, response){
-//     console.log(response);
-// });
